079087f64f64dcd4cfdde48e7534d75f10ab2b55
piotr <piotrkowal2014@gmail.com>
2025-01-18 18:18:37 +0100

    * small cleaning * moved loosely code to internal method * added searching for dir structure * created method for filtering book files

--- tagsFixer/MainApp/MainApp/Program.cs
+++ tagsFixer/MainApp/MainApp/Program.cs
@@ -1,34 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 
-Console.WriteLine("Hello, World!");
-
-
-
-
-string containerPath = @"C:\Users\piotr\notepad";
-Dictionary<string, IDictionary> hierarchy = new();
-IEnumerable<String> mainPathDirs = Directory.EnumerateDirectories(containerPath);
-IEnumerable<String> mainPathFiles = Directory.EnumerateFiles(containerPath);
-
-
+// Probably should be moved to config file or inp argument
 IEnumerable<string> blackList = [".obsidian", ".trash", "assets"];
+string notepadPath = @"C:\Users\piotr\notepad";
+string booksLocation = @"Zbiory\książki\ebooki\";
 
-foreach (var subdir in mainPathDirs)
+var notepadHierarchy = GetNotepadHierarchy(notepadPath, blackList);
+var books = GetBooksFiles(notepadHierarchy);
+
+
+IDictionary GetNotepadHierarchy(string containerPath, IEnumerable<string> ignoreList)
 {
-    if (IsDirBlacklisted(subdir)) continue;
-    var subdirDirs = GetDirsContent(subdir);
-    var subdirFiles = Directory.EnumerateFiles(subdir, "*", SearchOption.AllDirectories);
-    hierarchy[subdir] = new Dictionary<string, IEnumerable<string>>();
-    hierarchy[subdir].Add("files", subdirFiles);
+    Dictionary<string, IDictionary> hierarchy = new();
+    IEnumerable<String> mainPathDirs = Directory.EnumerateDirectories(containerPath);
+    IEnumerable<String> mainPathFiles = Directory.EnumerateFiles(containerPath);
+    Console.WriteLine("Checking main dir");
+
+
+    foreach (var subdir in mainPathDirs)
+    {
+        if (IsDirBlacklisted(subdir)) continue;
+        Console.WriteLine($"Checking {subdir} path...");
+        var subdirDirs = GetDirsContent(subdir);
+        var subdirFiles = Directory.EnumerateFiles(subdir, "*", SearchOption.AllDirectories);
+        hierarchy[subdir] = new Dictionary<string, IEnumerable<string>>();
+        hierarchy[subdir].Add("files", subdirFiles);
+    }
+
+    hierarchy["."] = new Dictionary<string, IEnumerable<string>>
+    {
+        { "files", mainPathFiles },
+        { "dirs", mainPathDirs }
+    };
+
+    return hierarchy;
+
+    IEnumerable<string> GetDirsContent(string name) => Directory.EnumerateDirectories(name);
+    bool IsDirBlacklisted(string dirName) => ignoreList.Any(path => dirName.Contains(path));
 }
 
+IEnumerable<string> GetBooksFiles(IDictionary dirTree)
+{
 
-Console.WriteLine();
-
-
-IEnumerable<string> GetDirsContent(string name) => Directory.EnumerateDirectories(name);
-bool IsDirBlacklisted(string dirName) =>  blackList.Any(path => dirName.Contains(path));
-
+    return [];
+}
 
