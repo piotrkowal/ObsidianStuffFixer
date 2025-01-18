a7c97f3fd1b356654dbbd3b393836a089817f74a
piotr <piotrkowal2014@gmail.com>
2025-01-18 23:12:42 +0100

    Cleanup not great ideas, pushed to just finding boks

--- tagsFixer/MainApp/MainApp/Program.cs
+++ tagsFixer/MainApp/MainApp/Program.cs
@@ -1,47 +1,33 @@
-﻿using System.Collections;
-
-// Probably should be moved to config file or inp argument
+﻿// Probably should be moved to config file or inp argument
 IEnumerable<string> blackList = [".obsidian", ".trash", "assets"];
 string notepadPath = @"C:\Users\piotr\notepad";
 string booksLocation = @"Zbiory\książki\ebooki\";
 
-var notepadHierarchy = GetNotepadHierarchy(notepadPath, blackList);
-var books = GetBooksFiles(notepadHierarchy);
+var notepadHierarchy = GetNotepadFilesList(notepadPath, blackList);
+var books = GetBooksFiles(notepadHierarchy, booksLocation);
 
-
-IDictionary GetNotepadHierarchy(string containerPath, IEnumerable<string> ignoreList)
+foreach (var book in books)
 {
-    Dictionary<string, IDictionary> hierarchy = new();
-    IEnumerable<String> mainPathDirs = Directory.EnumerateDirectories(containerPath);
-    IEnumerable<String> mainPathFiles = Directory.EnumerateFiles(containerPath);
-    Console.WriteLine("Checking main dir");
+    Console.WriteLine(book);
+}
 
+
+IEnumerable<string> GetNotepadFilesList(string containerPath, IEnumerable<string> ignoreList)
+{
+    List<string> fileList = new();
+    IEnumerable<String> mainPathDirs = Directory.EnumerateDirectories(containerPath).ToList();
 
     foreach (var subdir in mainPathDirs)
     {
         if (IsDirBlacklisted(subdir)) continue;
         Console.WriteLine($"Checking {subdir} path...");
-        var subdirDirs = GetDirsContent(subdir);
-        var subdirFiles = Directory.EnumerateFiles(subdir, "*", SearchOption.AllDirectories);
-        hierarchy[subdir] = new Dictionary<string, IEnumerable<string>>();
-        hierarchy[subdir].Add("files", subdirFiles);
+        var subdirFiles = Directory.EnumerateFiles(subdir, "*", SearchOption.AllDirectories).ToList();
+        fileList.AddRange(subdirFiles);
     }
 
-    hierarchy["."] = new Dictionary<string, IEnumerable<string>>
-    {
-        { "files", mainPathFiles },
-        { "dirs", mainPathDirs }
-    };
+    return fileList;
 
-    return hierarchy;
-
-    IEnumerable<string> GetDirsContent(string name) => Directory.EnumerateDirectories(name);
     bool IsDirBlacklisted(string dirName) => ignoreList.Any(path => dirName.Contains(path));
 }
 
-IEnumerable<string> GetBooksFiles(IDictionary dirTree)
-{
-
-    return [];
-}
-
+IEnumerable<string> GetBooksFiles(IEnumerable<string> dirTree, string booksPath) => dirTree.Where(x => x.Contains(booksPath));
