dd479337dd4dc39e2a398eed7f78d31b1ffbb506
piotr <piotrkowal2014@gmail.com>
2025-01-19 01:30:20 +0100

    First version of replacing lines with unnecessary |100 attribute in image source

--- tagsFixer/MainApp/MainApp/Program.cs
+++ tagsFixer/MainApp/MainApp/Program.cs
@@ -9,12 +9,27 @@ var books = GetBooksFiles(notepadHierarchy, booksLocation);
 foreach (var book in books)
 {
     Console.WriteLine(book);
-}
+    var lines = File.ReadAllLines(book).ToList();
 
+    var lineIndex = 0;
+    string splittedLine = string.Empty;
+    foreach (var line in lines)
+    {
+        if (line.StartsWith("cover: \"") && !line.EndsWith(".png]]\""))
+        {
+            splittedLine = line.Split('|').FirstOrDefault() + "]]\"";
+            Console.WriteLine(lineIndex+" "+splittedLine);
+            lines[lineIndex] = splittedLine;
+            break;
+        }
+        lineIndex++;
+    }
+    if (lineIndex != 0) File.WriteAllLines(book, lines);
+}
 
 IEnumerable<string> GetNotepadFilesList(string containerPath, IEnumerable<string> ignoreList)
 {
-    List<string> fileList = new();
+    List<string> fileList = [];
     IEnumerable<String> mainPathDirs = Directory.EnumerateDirectories(containerPath).ToList();
 
     foreach (var subdir in mainPathDirs)
